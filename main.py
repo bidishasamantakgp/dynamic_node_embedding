128839c827022651a4f20c3f46c3900adc14e659
bidishasamantakgp <samantatukai@gmail.com>
2019-04-23 10:47:27 +0530

    Code complete

--- main.py
+++ main.py
@@ -10,68 +10,33 @@ logging.basicConfig(format="[%(asctime)s] %(message)s", datefmt="%m%d %H:%M:%S")
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
-FLAGS = None
-placeholders = {
-    'dropout': tf.placeholder_with_default(0., shape=()),
-    'lr': tf.placeholder_with_default(0., shape=()),
-    'decay': tf.placeholder_with_default(0., shape=())
-    }
-
-def add_arguments(parser):
-    parser.register("type", "bool", lambda v: v.lower() == "true")
-    # network
-    parser.add_argument("--num_epochs", type=int, default=32, help="Number of epochs")
-    parser.add_argument("--learning_rate", type=float, default=0.00005, help="learning rate")
-    parser.add_argument("--decay_rate", type=float, default=1.0, help="decay rate")
-    parser.add_argument("--dropout_rate", type=float, default=0.00005, help="dropout rate")
-    parser.add_argument("--log_every", type=int, default=5, help="write the log in how many iterations")
-    parser.add_argument("--sample_file", type=str, default=None, help="directory to store the sample graphs")
-    parser.add_argument("--random_walk", type=int, default=5, help="random walk depth")
-    parser.add_argument("--z_dim", type=int, default=5, help="z_dim")
-    parser.add_argument("--nodes", type=int, default=5, help="node number")
-    parser.add_argument("--graph_file", type=str, default=None,
-                        help="The dictory where the training graph structure is saved")
-    parser.add_argument("--z_dir", type=str, default=None,
-                        help="The z values will be stored file to be stored")
-    parser.add_argument("--sample", type=bool, default=False, help="True if you want to sample")
-
-    parser.add_argument("--mask_weight", type=bool, default=False, help="True if you want to mask weight")
-
-    parser.add_argument("--out_dir", type=str, default=None,
-                        help="Store log/model files.")
-
-def create_hparams(flags):
-  """Create training hparams."""
-  return tf.contrib.training.HParams(
-      # Data
-      graph_file=flags.graph_file,
-      out_dir=flags.out_dir,
-      z_dir=flags.z_dir,
-      sample_file=flags.sample_file,
-      z_dim=flags.z_dim,
-
-      # training
-      learning_rate=flags.learning_rate,
-      decay_rate=flags.decay_rate,
-      dropout_rate=flags.dropout_rate,
-      num_epochs=flags.num_epochs,
-      random_walk=flags.random_walk,
-      log_every=flags.log_every,
-      nodes=flags.nodes,
-      mask_weight=flags.mask_weight,
-
-      #sample
-      sample=flags.sample
-      )
+def add_arguments():
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument('--rnn_size', type=int, default=3,
+                        help='size of RNN hidden state')
+    parser.add_argument('--latent_size', type=int, default=3,
+                        help='size of latent space')
+    parser.add_argument('--batch_size', type=int, default=3000,
+                        help='minibatch size')
+    parser.add_argument('--seq_length', type=int, default=100,
+                        help='RNN sequence length')
+    parser.add_argument('--num_epochs', type=int, default=100,
+                        help='number of epochs')
+    parser.add_argument('--save_every', type=int, default=500,
+                        help='save frequency')
+    parser.add_argument('--grad_clip', type=float, default=10.,
+                        help='clip gradients at this value')
+    parser.add_argument('--learning_rate', type=float, default=0.0005,
+                        help='learning rate')
+    parser.add_argument('--decay_rate', type=float, default=1.,
+                        help='decay of learning rate')
+    parser.add_argument('--chunk_samples', type=int, default=1,
+                        help='number of samples per mdct chunk')
+    return parser
 
 if __name__ == '__main__':
-    nmt_parser = argparse.ArgumentParser()
-    add_arguments(nmt_parser)
-    FLAGS, unparsed = nmt_parser.parse_known_args()
-    hparams = create_hparams(FLAGS)
-    
-    # loading the data from a file
-    adj, features, edges = load_data(hparams.graph_file, hparams.nodes)
-    model = VAEG()
-    model.initialize()
-    model.train()
+    parser = add_arguments()
+    args = parser.parse_args()
+    model = MPPModel(args)
+    model.train(args)
--- model.py
+++ model.py
@@ -5,7 +5,7 @@ class MPPModel():
     def __init__(self, args, sample=False):
 
         self.n = n
-        
+
 
         def tf_normal(y, mu, s):
             with tf.variable_scope('normal'):
@@ -38,8 +38,8 @@ class MPPModel():
         cell = MPPCell(args.chunk_samples, args.rnn_size, args.latent_size)
         self.cell = cell
 
-        self.input_data = tf.placeholder(dtype=tf.float32, shape=[args.batch_size, args.seq_length, 2*args.chunk_samples], name='input_data')
-        self.target_data = tf.placeholder(dtype=tf.float32, shape=[args.batch_size, args.seq_length, 2*args.chunk_samples],name = 'target_data')
+        self.input_data = tf.placeholder(dtype=tf.float32, shape=[args.batch_size, args.seq_length, 2 * args.chunk_samples], name='input_data')
+        self.target_data = tf.placeholder(dtype=tf.float32, shape=[args.batch_size, args.seq_length, 2 * args.chunk_samples],name = 'target_data')
         self.initial_state_c, self.initial_state_h = cell.zero_state(batch_size=args.batch_size, dtype=tf.float32)
 
 
@@ -49,18 +49,20 @@ class MPPModel():
             inputs = tf.reshape(inputs, [-1, 2*args.chunk_samples]) # (n_steps*batch_size, n_input)
 
             # Split data because rnn cell needs a list of inputs for the RNN inner loop
-            inputs = tf.split(axis=0, num_or_size_splits=args.seq_length, value=inputs) # n_steps * (batch_size, n_hidden)
+            inputs = tf.split(axis=0, num_or_size_splits=args.seq_length, value=inputs) # n_steps * (batch_size, n_hidden)     
+        
         flat_target_data = tf.reshape(self.target_data,[-1, 2*args.chunk_samples])
 
         self.target = flat_target_data
         self.flat_input = tf.reshape(tf.transpose(tf.stack(inputs),[1,0,2]),[args.batch_size*args.seq_length, -1])
         self.input = tf.stack(inputs)
+        
         # Get vrnn cell output
         outputs, last_state = tf.contrib.rnn.static_rnn(cell, inputs, initial_state=(self.initial_state_c,self.initial_state_h))
-        #print outputs
+        
         #outputs = map(tf.pack,zip(*outputs))
         outputs_reshape = []
-        names = ["enc_mu", "enc_sigma", "dec_mu", "dec_sigma", "dec_rho", "prior_mu", "prior_sigma"]
+        names = ["enc_mu", "enc_sigma", "dec_mu", "dec_sigma", "prior_mu", "prior_sigma", "lambda_association", "lambda_communication"]
         for n,name in enumerate(names):
             with tf.variable_scope(name):
                 x = tf.stack([o[n] for o in outputs])
@@ -68,7 +70,8 @@ class MPPModel():
                 x = tf.reshape(x,[args.batch_size*args.seq_length, -1])
                 outputs_reshape.append(x)
 
-        enc_mu, enc_sigma, dec_mu, dec_sigma, dec_rho, prior_mu, prior_sigma = outputs_reshape
+        enc_mu, enc_sigma, dec_mu, dec_sigma, prior_mu, prior_sigma, lambda_association, lambda_communication = outputs_reshape
+        
         self.final_state_c,self.final_state_h = last_state
         self.mu = dec_mu
         self.sigma = dec_sigma
@@ -89,10 +92,6 @@ class MPPModel():
         for t in tvars:
             print t.name
         grads = tf.gradients(self.cost, tvars)
-        #grads = tf.cond(
-        #    tf.global_norm(grads) > 1e-20,
-        #    lambda: tf.clip_by_global_norm(grads, args.grad_clip)[0],
-        #    lambda: grads)
         optimizer = tf.train.AdamOptimizer(self.lr)
         self.train_op = optimizer.apply_gradients(zip(grads, tvars))
 
@@ -100,7 +99,46 @@ class MPPModel():
         #TBD
 
 
-    def train(self):
-        #TBD
+    def train(self, args):
+        dirname = args.dirname
+        if not os.path.exists(dirname):
+            os.makedirs(dirname)
+
+        with open(os.path.join(dirname, 'config.pkl'), 'w') as f:
+            cPickle.dump(args, f)
+
+        ckpt = tf.train.get_checkpoint_state(dirname)
+        n_batches = 100
+        with tf.Session() as sess:
+            summary_writer = tf.summary.FileWriter('logs/' + datetime.now().isoformat().replace(':', '-'), sess.graph)
+            check = tf.add_check_numerics_ops()
+            merged = tf.summary.merge_all()
+            tf.global_variables_initializer().run()
+            saver = tf.train.Saver(tf.global_variables())
+            if ckpt:
+                saver.restore(sess, ckpt.model_checkpoint_path)
+                print "Loaded model"
+            start = time.time()
+            for e in xrange(args.num_epochs):
+                sess.run(tf.assign(model.lr, args.learning_rate * (args.decay_rate ** e)))
+                state = model.initial_state_c, model.initial_state_h
+                for b in xrange(n_batches):
+                    x, y = next_batch(args)
+                    feed = {model.input_data: x, model.target_data: y}
+                    train_loss, _, cr, summary, sigma, mu, input, target= sess.run(
+                            [model.cost, model.train_op, check, merged, model.sigma, model.mu, model.flat_input, model.target],
+                                                                 feed)
+                    summary_writer.add_summary(summary, e * n_batches + b)
+                    if (e * n_batches + b) % args.save_every == 0 and ((e * n_batches + b) > 0):
+                        checkpoint_path = os.path.join(dirname, 'model.ckpt')
+                        saver.save(sess, checkpoint_path, global_step=e * n_batches + b)
+                        print "model saved to {}".format(checkpoint_path)
+                    end = time.time()
+                    print "{}/{} (epoch {}), train_loss = {:.6f}, time/batch = {:.1f}, std = {:.3f}" \
+                        .format(e * n_batches + b,
+                                args.num_epochs * n_batches,
+                                e, args.chunk_samples * train_loss, end - start, sigma.mean(axis=0).mean(axis=0))
+        start = time.time()
+            
 
 
